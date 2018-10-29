ea0848937da45d4382f26dc90cc21f376c645582
Alexander Elbs <alex@segv.de>
2018-10-29 20:09:24 +0100

    Adapted doorsign

--- esp32/doorsignEPD/doorsignEPD.ino
+++ esp32/doorsignEPD/doorsignEPD.ino
@@ -2,7 +2,7 @@
 #include <Basecamp.hpp>
 
 //Define yout display type here: 2.9, 4.2 or 7.5 inches are supported:
-#define DISPLAY_TYPE '2.9'
+#define DISPLAY_TYPE '7.5'
 #define FactorSeconds 1000000
 #define BASECAMP_NOMQTT
 
@@ -27,6 +27,9 @@ GxEPD_Class display(io, 16, 4);
 int value = 0;
 bool connection = false;
 bool production = false;
+int sleeptime = 0;
+
+static RTC_DATA_ATTR char id;
 
 void setup() {
   iot.begin();
@@ -114,13 +117,14 @@ void setup() {
 
   }
 
+  sleeptime = iot.configuration.get("ImageWait").toInt();
+  Serial.print("id: "); Serial.println(id, DEC);
 }
 
 void loop() {
 
   if (connection == true) {
     String url =  iot.configuration.get("ImageAddress");
-    boolean currentLineIsBlank = true;
     WiFiClient client;
     delay(5000);
     ++value;
@@ -137,72 +141,119 @@ void loop() {
                  "Host: " + iot.configuration.get("ImageHost") + "\r\n" +
                  "Connection: close\r\n\r\n");
 
+    for(int i=0; i<50; i++) {
+      if (client.available()) {
+        break;
+      } else {
+        Serial.println("waiting for client");
+        delay(100);
+      }
+    }
+
     int x = 0;
     int y = 0;
-    int start = 0;
-    bool data = false;
-    String header; 
-
-    display.eraseDisplay();
+    String header;
+    int state = 0;
+    int count = 0;
 
     while (client.available()) {
       char byte = client.read();
+      count++;
 
-      if(data == false){
-        header +=  byte;
-       }
+      if (state == 0) { // reading header
+        header += byte;
 
-      if (byte == '\n' && currentLineIsBlank && data == false) {
-        data = true;
-        
-        if(header.indexOf("X-productionMode: false")>0) {
+        if (byte == '\n') {
+          state = 1; // found a new line
+        }
+
+      } else if (state == 1) { // newline was found, still header
+        header += byte;
+
+        if (byte == '\n') {
+          state = 2; // found second new line (blank line), now data
+
+          if (header.indexOf("X-productionMode: false") > 0) {
             iot.configuration.set("ProductionMode", "false");
             production=false;
-        }
-         if(header.indexOf("X-productionMode: true")>0) {
+          }
+          if (header.indexOf("X-productionMode: true") > 0) {
             iot.configuration.set("ProductionMode", "true");
             production=true;
-        }
-      }
-      if (byte == '\n' && data == false) {
-        currentLineIsBlank = true;
-      } else if (byte != '\r' && data == false) {
-        currentLineIsBlank = false;
-      }
+          }
 
-      if (data) {
-        if (start < 1) {
-          start++;
+        } else if (byte == '\r') {
+          // keep state
         } else {
+          state = 0; // more header, no blank line
+        }
 
-          for (int b = 7; b >= 0; b--) {
-            int bit = bitRead(byte, b);
+      } else if (state == 2) { // high byte of delay in seconds
+        sleeptime = byte * 256;
+        state = 3;
 
-            if (bit == 1) {
-              display.drawPixel(x, y, GxEPD_BLACK);
-            } else {
-              display.drawPixel(x, y, GxEPD_WHITE);
-            }
+      } else if (state == 3) { // low byte of delay in seconds
+        sleeptime += byte;
+        state = 4;
 
-            x++;
+      } else if (state == 4) { // id byte
+        if (id == byte) {
+          // no display update needed
+          state = 5;
+          Serial.println("no id change, no image update");
+          Serial.println(id);
+        } else {
+          // update display with new image
+          Serial.print("id changed, image update needed, old id: ");
+          Serial.print(id, DEC);
+          Serial.print(", new id: ");
+          Serial.println(byte, DEC);
+          display.eraseDisplay();
+          state = 6;
+          id = byte;
+        }
 
-            if  (x == GxEPD_WIDTH) {
-              y++;
-              x = 0;
-            }
+      } else if (state == 5) { // read image, but discard
+          x+=8;
+
+          if (x == GxEPD_WIDTH) {
+            y++;
+            x = 0;
+          }
+
+      } else if (state == 6) { // update display with new image
+        for (int b = 7; b >= 0; b--) {
+          int bit = bitRead(byte, b);
+
+          if (bit == 1) {
+            display.drawPixel(x, y, GxEPD_BLACK);
+          } else {
+            display.drawPixel(x, y, GxEPD_WHITE);
+          }
+
+          x++;
+
+          if (x == GxEPD_WIDTH) {
+            y++;
+            x = 0;
           }
         }
       }
     }
-    display.update();
-    Serial.println("Image loaded.");
+    Serial.print("Read bytes from server: "); Serial.println(count);
+    Serial.print("x="); Serial.println(x);
+    Serial.print("y="); Serial.println(y);
 
-
+    if (state == 6) {
+      display.update();
+      Serial.println("image updated");
+    }
   }
-  if (production == true) {
 
-    int SleepTime = iot.configuration.get("ImageWait").toInt();
-    esp_sleep_enable_timer_wakeup(SleepTime * FactorSeconds);
+  if (production == true) {
+    esp_sleep_enable_timer_wakeup(sleeptime * FactorSeconds);
+    Serial.print("Sleeping for (seconds): ");
+    Serial.println(sleeptime);
     Serial.println("Going to sleep now...");
     esp_deep_sleep_start();
   } else {
